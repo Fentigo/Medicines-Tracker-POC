c61deb540429bed6555dd5367ad03432ebf3a4cb
Fentigo <fentyj96@gmail.com>
2023-08-08 15:18:52 +0100

    features updated

--- User_interface.py
+++ User_interface.py
@@ -1,3 +1,5 @@
+# there's a cleaner way to do this using modules
+from datetime import datetime, timedelta
 import sqlite3
 connect = sqlite3.connect('inventory db')
 cursor = connect.cursor()
@@ -6,52 +8,97 @@ def display_menu():
     print("1. Add Medicine")
     print("2. View Medicines")
     print("3. View Medicines Expiring Soon")
-    print("4. Exit")
-    choice = input("Enter your choice (1/2/3/4): ")
+    print("4. Update Expiry Date")
+    print("5. Update Quantity")
+    print("6. Delete Medicine")
+    print("7. Search Medicine")
+    print("8. Check Alerts")
+    print("9. Exit")
+    choice = input("Enter your choice (1/2/3/4/5/6/7/8): ")
     return choice
 
 
-def add_medicine():
-    print(" Enter the following information")
-    name = input('insert medicine name')
-    batch_number = input ('inser batch number')
-    quantity = int(input('Enter Quantity here'))
-    expiry_date = input('Enter date here YYYY-MM-DD')
-    cursor.execute ("INSERT INTO medicines (name, batch_number, quantity, expiry_date) VALUES (?, ?, ?, ?)", 
-                    (name, batch_number, quantity, expiry_date))
-    connect.commit()
-    connect.close
-    pass
-
-def view_medicine():
-    cursor.execute("SELECT * FROM medicines")
-    rows = cursor.fetchall()
-
-    print("Medicine ID | Name    | Batch Number | Quantity | Expiry Date")
-    print ("--------------------------------------------")
-    for row in rows: 
-        print(f"{row[0]:11}  | {row[1]:11}  | {row[2]:12}  |{row[3]:8}  | {row[4]}")
-    connect.commit()
-    connect.close
-    pass
-
-def view_expiring_medicine():
-    #placeholder 
-    pass
 def main():
+    # i want to implement a loop here that is always checking for expiry alerts. 
+    display_menu()
+        
     while True:
         choice = display_menu()
-        
         if choice == '1':
-            add_medicine()
+            try:
+                from add_medicine import add_medicine
+                add_medicine()
+            except ValueError:
+                print("Invalid input. Please enter valid details")
+            except sqlite3.Error as e:
+                print(f"An error occurred: {e}")
         elif choice == '2':
-            view_medicines()
+            try: 
+                from view_medicine import view_medicines
+                view_medicines()
+            except ValueError:
+                print("Invalid input. Please enter valid details")
+            except sqlite3.Error as e:
+                print(f"An error occurred: {e}")
         elif choice == '3':
-            view_expiring_medicines()
+            try:
+                from expiring_medicines import view_expiring_medicine
+                view_expiring_medicine()
+            except ValueError:
+                 print("Invalid input. Please enter valid details")
+            except sqlite3.Error as e:
+                print(f"An error occurred: {e}")
         elif choice == '4':
-            print("Exiting the Pharmacy Inventory Management System.")
+            try: 
+                from update_expiry_date import update_expiry_date
+                update_expiry_date()
+            except ValueError:
+                 print("Invalid input. Please enter valid details")
+            except sqlite3.Error as e:
+                print(f"An error occurred: {e}")
+        elif choice == '5':
+            try: 
+                from update_quantity import update_medicine_quantity
+                update_medicine_quantity()
+            except ValueError:
+                 print("Invalid input. Please enter valid details")
+            except sqlite3.Error as e:
+                print(f"An error occurred: {e}")
+        elif choice == '6':
+            try:
+                from delete import delete_medicine
+                delete_medicine()
+            except ValueError:
+                 print("Invalid input. Please enter valid details")
+            except sqlite3.Error as e:
+                print(f"An error occurred: {e}")
+        elif choice == '7':
+            try:
+                from search_medicine import search
+                search()
+            except ValueError:
+                 print("Invalid input. Please enter valid details")
+            except sqlite3.Error as e:
+                print(f"An error occurred: {e}")
+        elif choice == '8':
+            try:
+                from check_expired_medicines import check_expired_medicines
+                check_expired_medicines()
+                print("Exiting the Pharmacy Inventory Management System.")
+            except ValueError:
+                 print("Invalid input. Please enter valid details")
+            except sqlite3.Error as e:
+                print(f"An error occurred: {e}")
+        elif choice == '9':
+            try:
+                from check_expired_medicines import check_expired_medicines
+                check_expired_medicines()
+                print("Exiting the Pharmacy Inventory Management System.")
+            except ValueError:
+                 print("Invalid input. Please enter valid details")
+            except sqlite3.Error as e:
+                print(f"An error occurred: {e}")
             break
         else:
             print("Invalid choice. Please try again.")
-
-display_menu()
+main()
